a1eb401b896955f2efc8070d19dce48f5342a5b0
mahaboobk <mahaboob.lotus@gmail.com>
2025-07-19 17:47:58 +0530

    ActionCable Update Receiving Actions

--- app/channels/appointment_channel.rb
+++ app/channels/appointment_channel.rb
@@ -3,6 +3,13 @@ class AppointmentChannel < ApplicationCable::Channel
     stream_from "appointments"
   end
 
+  def receive(data)
+    ActionCable.server.broadcast("appointments", {
+      action: data['action'],
+      data: data['data']
+    })
+  end
+  
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
+  config.action_cable.allowed_request_origins = [/ws:\/\/*/, /wss:\/\/*/]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
