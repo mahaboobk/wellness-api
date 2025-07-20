6798538c9de3ee78cb050033918ae03c475165b2
mahaboobk <mahaboob.lotus@gmail.com>
2025-07-21 03:48:57 +0530

    launch page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,11 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
+  #  "wellness-api-rotv.onrender.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
-  #
+  config.hosts << "wellness-api-rotv.onrender.com"
+
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
